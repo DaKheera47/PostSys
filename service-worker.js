b62d28757c7caf288d318fef113e469959265236
DaKheera47 <shaheergoodboy@gmail.com>
2020-09-27 23:53:02 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PostSys/precache-manifest.a4bacd092990eb0e68002a1f21046473.js"
+  "/PostSys/precache-manifest.6ac6cef627937edcdefd4f3e0683ae3c.js"
 );
 
 self.addEventListener('message', (event) => {
