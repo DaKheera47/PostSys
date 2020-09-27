cae222376f74874cf1045de4557ce9791a6fc491
DaKheera47 <shaheergoodboy@gmail.com>
2020-09-27 17:50:24 +0500

    Updates

---
+++ precache-manifest.265a8e09cb746a7eee69ea58af96758b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd51363736b6d7a725dd820e6ae66e0",
+    "url": "/PostSys/index.html"
+  },
+  {
+    "revision": "5083116495c6824447f0",
+    "url": "/PostSys/static/css/3.21a56829.chunk.css"
+  },
+  {
+    "revision": "662e9a479e4ce10b7432",
+    "url": "/PostSys/static/css/main.9ec3064d.chunk.css"
+  },
+  {
+    "revision": "3c0b8a6a6e620e478310",
+    "url": "/PostSys/static/js/2.a0988861.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/PostSys/static/js/2.a0988861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5083116495c6824447f0",
+    "url": "/PostSys/static/js/3.b6224236.chunk.js"
+  },
+  {
+    "revision": "662e9a479e4ce10b7432",
+    "url": "/PostSys/static/js/main.518017e3.chunk.js"
+  },
+  {
+    "revision": "e7e2e2497a344d2c76aa",
+    "url": "/PostSys/static/js/runtime-main.c69578e8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a73959c46c1a66628f43f671c6a94e0.js"
+  "/PostSys/precache-manifest.265a8e09cb746a7eee69ea58af96758b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PostSys/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
