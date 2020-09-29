ffb9b06c0d3c899ad693aa94b7876bd9e324458b
DaKheera47 <shaheergoodboy@gmail.com>
2020-09-29 19:32:57 +0500

    Adding Sign up and Sign in using auth0

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ import { HashRouter } from "react-router-dom";
 const domain = process.env.REACT_APP_DOMAIN;
 const clientId = process.env.REACT_APP_CLIENT_ID;
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    // dev code
+    const redirectURI = "http://localhost:3000/#/workspace";
+} else {
+    // production code
+    const redirectURI = "https://dakheera47.github.io/PostSys/#/workspace";
+}
+
 ReactDOM.render(
-    <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirectUri="http://localhost:3000/#/workspace"
-    >
+    <Auth0Provider domain={domain} clientId={clientId} redirectUri={redirectURI}>
         <HashRouter>
             <React.StrictMode>
                 <App />
